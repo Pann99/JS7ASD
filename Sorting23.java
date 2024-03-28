daa53f5c81fa2f2cd2cb39941960ad09fc904b71
github_Pandaaa00 <pandya.cahya99@gmail.com>
2024-03-29 05:20:05 +0700

    PRAKTIKUM 7

--- Sorting23.java
+++ Sorting23.java
@@ -1,16 +1,47 @@
 public class Sorting23 {
-    public static void sequentialSearch(int[] arr, int key) {
+
+    public static void sequentialSearch(int arr[], int key){
         for (int i = 0; i < arr.length; i++) {
-            if (i == key) {
-                System.out.println("Data ditemukan pada indeks ke-" + i);
+            if (arr[i] == key) {
+                System.out.println("data  ditemukan pada indeks ke-" + i);
+                return;
             }
         }
 
-        System.out.println("Data tidak ditemukan");
+
+        System.out.println("data tidak ditemukan");
     }
 
+    public static int binarySearchAsc(int[] arr, int key){
+        int start = 0, end = arr.length -1;
+
+        while (start <= end) {
+            int mid = start + (end-start)/2;
+
+            if (arr[mid] == key) {
+                return mid;
+            }
+
+            if (arr[mid] < key) {
+                start = mid + 1;
+            }else{
+                end = mid -1;
+            }
+        }
+        return -1;
+    }
     public static void main(String[] args) {
-        int[] daftarNilai = { 10, 5, 20, 15, 5, 45 };
+        int[] daftarNilai = {10,5,20,15,5,45};
         sequentialSearch(daftarNilai, 5);
+
+        int[] sortedNilai = {5,5,10,20,30,40,50};
+        int index = binarySearchAsc(sortedNilai, 10);
+
+        if (index != -1) {
+            System.out.println("Data Ditemukan pada indeks ke-" + index );
+
+        }else{
+            System.out.println("data tidak ditemukan");
+        }
     }
 }
